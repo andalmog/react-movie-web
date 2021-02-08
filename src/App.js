71b7fd2450aea1807fad03452528327c30b8d677
dal <andalgom@gmail.com>
2021-02-08 13:02:08 +0900

    2.4 PropTypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 // import Potato from "./Potato";
 /** 3.
  * return 시 <Potato />를 작성하면 import해서 rendering 가능
@@ -8,35 +9,52 @@ import React from "react";
   * jsx의 두번째 특징
   * component에 정보를 보낼 수 있음
   */
-function Food({ name, picture }){ /** 5. component는 대문자로 시작 */
+function Food({ name, picture, rating }){ /** 5. component는 대문자로 시작 */
   /** 6. { fav }
    * ES6 문법으로 props.fav == { fav }
    */
   //console.log(props.fav);
   return <div>
             <h1>I like {name}</h1>
+            <h4>{rating}/5.0</h4>
             <img src={picture} alt={name}/>
         </div>
 }
 
+/** 9. propTypes
+ * 반드시 propTypes 이라고 명명해야함.
+ * isRequired : 필수값
+ */
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
+
+/** 8. npm i prop-type
+ * component가 적절한지 확인해주는 것
+ */
 const foodILike = [
   {
     id:1,
     name: "Kimchi", 
-    image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+    image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 5.0
   },
   {
     id:2,
     name: "Ramen", 
-    image: "https://i.pinimg.com/originals/20/58/fd/2058fd4a6bce10ada1bce68c229e6ca4.jpg"
+    image: "https://i.pinimg.com/originals/20/58/fd/2058fd4a6bce10ada1bce68c229e6ca4.jpg",
+    rating: 4.9
   },
   {
     id:3,
     name: "candy", 
-    image: "https://i.pinimg.com/originals/04/b9/0c/04b90c32869d09d91e8447c3f17b10c4.jpg"
+    image: "https://i.pinimg.com/originals/04/b9/0c/04b90c32869d09d91e8447c3f17b10c4.jpg",
+    rating: 4.8
   }
 ];
-/** 6. map
+/** 7. map
  * map 함수는 arr의 item을 하나씩 꺼내오고
  * item의 개수만큼 돈 후에 return했던 값 들을 한번에 arr로 가져옴
  * map 함수의 매개변수로는 사용자함수를 보내면 됨.
@@ -54,7 +72,7 @@ function App() {
     <div>
       <h1>Hello</h1>
       {foodILike.map(dish => 
-        <Food key={dish.id} name={dish.name} picture={dish.image} />)
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />)
       }
     </div>
   );
